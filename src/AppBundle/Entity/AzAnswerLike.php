1755c9b34987da18b11e262f0fdef1d35d94b4b9
matzheng <99284419@qq.com>
2015-12-07 18:08:30 +0800

    commit20151207

---
+++ src/AppBundle/Entity/AzAnswerLike.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * AzAnswerLike
+ */
+class AzAnswerLike
+{
+    /**
+     * @var integer
+     */
+    private $time;
+
+    /**
+     * @var integer
+     */
+    private $mid;
+
+    /**
+     * @var integer
+     */
+    private $aid;
+
+
+    /**
+     * Get time
+     *
+     * @return integer
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
+
+    /**
+     * Set mid
+     *
+     * @param integer $mid
+     *
+     * @return AzAnswerLike
+     */
+    public function setMid($mid)
+    {
+        $this->mid = $mid;
+
+        return $this;
+    }
+
+    /**
+     * Get mid
+     *
+     * @return integer
+     */
+    public function getMid()
+    {
+        return $this->mid;
+    }
+
+    /**
+     * Set aid
+     *
+     * @param integer $aid
+     *
+     * @return AzAnswerLike
+     */
+    public function setAid($aid)
+    {
+        $this->aid = $aid;
+
+        return $this;
+    }
+
+    /**
+     * Get aid
+     *
+     * @return integer
+     */
+    public function getAid()
+    {
+        return $this->aid;
+    }
+}
+

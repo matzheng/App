98b3081df2fbde4ec2154da61ed0aea53f6c1134
matzheng <99284419@qq.com>
2016-01-13 20:30:04 +0800

    chkin

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -27,10 +27,11 @@ class DefaultController extends Controller
         else
             $page = (int)$page;
         $em = $this->getDoctrine()->getManager();
-        $sql = "
-        select * from (
-        select a.tid,a.title, a.detail,a.mid,a.qtypes,ifnull(b.uname,'') as uname,b.face,ifnull(b.product,'') product,ifnull(e.fid,0) as myfav,ifnull(c.favs,0) as favs from az_topic  a left join dede_member b on a.mid=b.mid left join (select tid, count(fid) as favs from az_member_fav group by tid) c on a.tid=c.tid left join (select fid,tid from az_member_fav where mid=".$ck.") e on a.tid=e.tid order by a.tid desc
-        ) a limit ".(($page - 1) * 10).",10";
+        //$sql = "
+        //select * from (
+        //select a.tid,a.title, a.detail,a.mid,a.qtypes,ifnull(b.uname,'') as uname,b.face,ifnull(b.product,'') product,ifnull(e.fid,0) as myfav,ifnull(c.favs,0) as favs from az_topic  a left join dede_member b on a.mid=b.mid left join (select tid, count(fid) as favs from az_member_fav group by tid) c on a.tid=c.tid left join (select fid,tid from az_member_fav where mid=".$ck.") e on a.tid=e.tid order by a.tid desc
+        //) a limit ".(($page - 1) * 10).",10";
+        $sql = "select a.tid,a.title, a.detail,a.mid,a.qtypes,ifnull(b.uname,'') as uname,b.face,ifnull(b.product,'') product,ifnull(e.fid,0) as myfav,ifnull(c.favs,0) as favs from az_topic  a left join dede_member b on a.mid=b.mid left join (select tid, count(fid) as favs from az_member_fav group by tid) c on a.tid=c.tid left join (select fid,tid from az_member_fav where mid=".$ck.") e on a.tid=e.tid order by a.tid desc";
         $q = $em->getConnection()->prepare($sql);
         $q->execute();
 
